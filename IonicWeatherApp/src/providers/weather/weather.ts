b4d3d42b450d0940c990fb3fbf34f9de5fd8c178
LARS BERG <larsgbr@gmail.com>
2017-11-09 14:37:33 -0600

    created an Ionic Tic Tac Toe game

--- IonicWeatherApp/src/providers/weather/weather.ts
+++ IonicWeatherApp/src/providers/weather/weather.ts
@@ -4,7 +4,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class WeatherProvider {
-  apiKey = 'PUTYOURAPIKEYHERE';
+  apiKey = '774236ce710cedd2';
   url;
 
   constructor(public http: Http) {
